ac1ee500d1b72633d2858eb859b239a05b0c9da3
legn11 <luislegn11@gmail.com>
2025-10-06 20:19:31 -0500

    Parcial 2

---
+++ PARCIALES/PARCIAL_2/clases.php
@@ -0,0 +1,136 @@
+<?php
+// Archivo: clases.php
+
+interface Inventariable{
+    public function obtenerInformacionInventario():string;
+}
+
+abstract class Producto implements Inventariable{
+    public $id;
+    public $nombre;
+    public $descripcion;
+    public $estado;
+    public $stock;
+    public $fechaIngreso;
+    public $categoria;
+
+    public function __construct($datos) {
+        foreach ($datos as $clave => $valor) {
+            if (property_exists($this, $clave)) {
+                $this->$clave = $valor;
+            }
+        }
+    }
+}
+
+class ProductoElectronico extends Producto{
+    public $garantiaMeses;
+
+    public function obtenerInformacionInventario(): string{
+        return "Meses de garantia: " . $this-> garantiaMeses;
+    }
+}
+
+class ProductoAlimento extends Producto {
+    public $fechaVencimiento;
+    public function obtenerInformacionInventario(): string{
+        return "fecha de vencimiento: " . $this-> fechaVencimiento;
+    }
+}
+
+class ProductoRopa extends Producto {
+    public $talla;
+        public function obtenerInformacionInventario(): string{
+        return "talla : " . $this-> talla;
+    }
+}
+
+class GestorInventario {
+    private $items = [];
+    private $rutaArchivo = 'productos.json';
+
+    public function obtenerTodos() {
+        if (empty($this->items)) {
+            $this->cargarDesdeArchivo();
+        }
+        return $this->items;
+    }
+//---=
+    private function cargarDesdeArchivo() {
+        if (!file_exists($this->rutaArchivo)) {
+            return;
+        }
+        
+        $jsonContenido = file_get_contents($this->rutaArchivo);
+        $arrayDatos = json_decode($jsonContenido, true);
+        
+        if ($arrayDatos === null) {
+            return;
+        }
+        
+        foreach ($arrayDatos as $datos) {
+            //$this->items[] = new Producto($datos);
+            $producto = null;
+            if($datos['categoria']==="electronico"){
+                $producto = new ProductoElectronico($datos);
+                $this->items[] = $producto;
+            } elseif($datos['categoria']==="alimento"){
+                $producto = new ProductoAlimento($datos);
+                $this->items[] = $producto;
+            } elseif ($datos['categoria']==="ropa"){
+                $producto = new ProductoRopa($datos);
+                $this->items[] = $producto;
+            }
+        }
+        return $this->items;
+    }
+
+    public function agregar($nuevoProducto){
+        $this->cargarDesdeArchivo();
+        if($nuevoProducto['categoria']==="electronico"){
+                $producto = new ProductoElectronico($nuevoProducto);
+            } elseif($nuevoProducto['categoria']==="alimento"){
+                $producto = new ProductoAlimento($nuevoProducto);
+            } elseif ($nuevoProducto['categoria']==="ropa"){
+                $producto = new ProductoRopa($nuevoProducto);
+            }
+         $producto->id = $this->obtenerMaximoId()+1;
+         $producto->fechaIngreso = date("Y-m-d");
+         $this->items[] = $producto;
+         $this->persistirEnArchivo();
+     }
+
+     public function eliminar($idProducto) {
+        foreach ($this->productos as $indice => $producto) {
+            if ($producto['id'] == $idProducto) {
+                unset($this->productos[$indice]);
+                $this->productos = array_values($this->productos); 
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function persistirEnArchivo() {
+        $arrayParaGuardar = array_map(function($item) {
+            return get_object_vars($item);
+        }, $this->items);
+        
+        file_put_contents(
+            $this->rutaArchivo, 
+            json_encode($arrayParaGuardar, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
+        );
+    }
+
+    public function obtenerMaximoId() {
+        if (empty($this->items)) {
+            return 0;
+        }
+        
+        $ids = array_map(function($item) {
+            return $item->id;
+        }, $this->items);
+        
+        return max($ids);
+    }
+}
